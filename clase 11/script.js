7d430849a95851d8efdd2273e5f116616807d7c0
Juan pablo Dominguez <juanpdominguez97@gmail.com>
2025-06-28 16:35:17 -0300

    filtrado de personajes listo

--- clase 11/script.js
+++ clase 11/script.js
@@ -1,7 +1,7 @@
 var tbodyMostrarPersonajes = document.getElementById('mostrarPersonajes');
-function mostrarPersonajes(datosJSON){
-    
-    var listaPersonajes = datosJSON.results;
+
+function mostrarPersonajes(listaPersonajes){
+    tbodyMostrarPersonajes.innerHTML = '';
 
     for(var i=0 ; i<listaPersonajes.length ; i++){
 
@@ -11,19 +11,16 @@ function mostrarPersonajes(datosJSON){
             `
                 <tr> 
                 <td>${personaje.id}</td>
-                <td><img src="${personaje.image}" alt="Foto de Rick Sanchez" style="width: 50px; height: 50px;" /></td>
+                <td><img src="${personaje.image}"/></td>
                 <td>${personaje.name}</td>
                 <td>${personaje.status}</td>
                 <td>${personaje.species}</td>
                 <td>${personaje.gender}</td>
                 </tr>
-            `;
+            `;}
 
-    }
-    
 
 }
-
 
 
 var btnObtenerTodos = document.getElementById('btnObtenerTodos')
@@ -33,9 +30,75 @@ var btnObtenerTodos = document.getElementById('btnObtenerTodos')
                                 }).then(function(respuesta){
                                     return respuesta.json();
                                 }).then(function(datosJson){
-                                    mostrarPersonajes(datosJson);
+                                    mostrarPersonajes(datosJson.results);
                                 }).catch(function(error){
                                     console.log(error);
                                 });
                         });
                         
+
+var formFiltrarPersonajes = document.getElementById('formFiltrarPersonajes').addEventListener('submit',
+    function(e){//?name=rick&status=alive
+        e.preventDefault();
+
+        var parametrosConsulta = '';
+        boolNombre = false;
+        boolStatus = false;
+        boolSpecies = false;
+        boolType = false;
+        boolGender = false;
+
+        if(document.getElementById('name').value != '' ){
+            parametrosConsulta += ( 'name' + '=' + document.getElementById('name').value )
+            boolNombre = true;
+        }
+
+        if(  document.getElementById('status').value != ''){
+            if(boolNombre){
+                parametrosConsulta += '&';
+            }
+            parametrosConsulta += ( 'status' + '=' + document.getElementById('status').value )
+            boolStatus = true;
+        }
+
+        if(document.getElementById('species').value != ''){
+            if(boolStatus || boolNombre){
+                parametrosConsulta += '&';
+            }
+            parametrosConsulta += ( 'species' + '=' + document.getElementById('species').value )
+            boolSpecies = true;
+        }
+
+        if(document.getElementById('type').value != ''){
+            if(boolSpecies || boolStatus || boolNombre){
+                parametrosConsulta += '&';
+            }
+            parametrosConsulta += ( 'type' + '=' + document.getElementById('type').value )
+            boolType = true;
+        }
+
+        if(document.getElementById('gender').value != ''){
+            if(boolSpecies || boolStatus || boolNombre ||boolType){
+                parametrosConsulta += '&';                
+            }
+            parametrosConsulta += ( 'gender' + '=' + document.getElementById('gender').value )
+        }
+
+        if(parametrosConsulta != ''){
+            buscarPersonajesFiltrados('?' + parametrosConsulta);
+        } else
+        alert('F');
+    }
+)
+
+function buscarPersonajesFiltrados(parametrosConsulta){
+        fetch('https://rickandmortyapi.com/api/character/'+ parametrosConsulta, {
+            method: 'GET'
+        }).then(function(promise){
+            return promise.json();
+        }).then(function(personajes){
+            mostrarPersonajes(personajes.results);
+        }).catch(function(error){
+            alert('error, personaje no encontrado');
+        })
+}
